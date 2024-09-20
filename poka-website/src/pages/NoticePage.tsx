57230b64250de2c2e7cdcbd7bbb23a03693fdacf
whysw <35984562+YangSeungWon@users.noreply.github.com>
2024-09-20 21:12:31 +0900

    add notice

--- poka-website/src/pages/NoticePage.tsx
+++ poka-website/src/pages/NoticePage.tsx
@@ -21,6 +21,18 @@ const notices: Notice[] = [
     title: "리그오브레전드 라이브 시작했습니다.",
     content: "Link: https://www.youtube.com/watch?v=4BVrzDfgcVQ",
     timestamp: new Date('2024-09-20T21:08:00'),
+  },
+  {
+    id: 3,
+    title: "2일차 경기 일정/장소 변경",
+    content: "* 야구: 기존 공지된 우천시 계획과 동일하게 진행\n* 과학퀴즈: 대강당으로 장소 변경\n* 농구: 체육관 실내에서 그대로 진행\n* 푸드트럭: 1일차와 동일한 장소에서 진행\n* 기획부스 및 교류부스: 1일차와 동일한 학생회관에서 진행\n* 폐막식 및 교류공연: 대강당으로 장소 변경",
+    timestamp: new Date('2024-09-20T21:12:00'),
+  },
+  {
+    id: 4,
+    title: "리그오브레전드 시작은 21시 20분",
+    content: "Link: https://www.youtube.com/watch?v=4BVrzDfgcVQ",
+    timestamp: new Date('2024-09-20T21:12:00'),
   }
   // 여기에 더 많은 공지사항을 추가할 수 있습니다.
 ];
